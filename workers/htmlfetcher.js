9d9384322d04ee172600107b2a308cf6033b454b
bkim121 <bkim121@gmail.com>
2017-09-15 20:44:36 -0700

    Got HMTLFetcher To Work

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,28 @@
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
+const path = require('path');
+const helpers = require('../helpers/archive-helpers');
+
+
+const CronJob = require('cron').CronJob;
+
+
+
+var job = new CronJob({
+  cronTime: '10 * * * * *',
+  onTick: function() {
+    let newUrls = [];
+    
+    helpers.readListOfUrls(array => array.forEach(el => {
+      helpers.isUrlArchived(el, (archived) => {
+        if (!archived) {
+          // newUrls.push(el);
+          helpers.downloadUrls([el]);
+        }
+      });
+    }));
+  },
+  start: false,
+  timeZone: 'America/Los_Angeles'
+});
+job.start();
