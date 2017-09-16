1e6ab63658cfcd37b6c0da4ec4a361d28c61bd45
bkim121 <bkim121@gmail.com>
2017-09-15 18:34:38 -0700

    refactoring

--- web/request-handler.js
+++ web/request-handler.js
@@ -4,38 +4,60 @@ var fs = require('fs');
 var parseBody = require('parse-body');
 
 var archive = require('../helpers/archive-helpers');
-// var sendResponse = function(req, res) {
-  
-// };
 
-// var sendFile = function () {
-  
-// };
+const gets = {
+  'home': (req, res) => fs.readFile(archive.paths.siteAssets + '/index.html', (err, data) => res.end(data)),
+  'search': (req, res) => {
+    fs.exists(archive.paths.archivedSites + req.url, (exists) => {
+      if (exists) {
+        fs.readFile(archive.paths.archivedSites + req.url, (err, data) => res.end(data));
+      } else {
+        res.writeHead(404, 'Not Found');
+        res.end();
+      }
+    }); 
+  },
+};
 
-exports.handleRequest = function (req, res) {
-
-  if (req.method === 'GET') {
-    if (req.url === '/' || req.url === '') {
-      fs.readFile(archive.paths.siteAssets + '/index.html', function (err, data) {
-        res.end(data);  
-      });
-    } else {
-      fs.exists(archive.paths.archivedSites + req.url, (exists) => {
-        if (exists) {
-          fs.readFile(archive.paths.archivedSites + req.url, (err, data) => res.end(data));
-        } else {
-          res.writeHead(404, 'Not Found');
-          res.end();
-        }
-      }); 
-    }
-  } else if (req.method === 'POST') {
+const posts = {
+  'write': (req, res) => {
     parseBody(req, 1e6, (err, body) => {
       fs.writeFile(archive.paths.list, body.url + '\n', () => {
         res.writeHead(302, 'Found');
         res.end();
       });
-    }); 
+    });
+  }
+};
+
+
+
+const methods = {
+  'GET': (req, res) => {
+    if (req.url === '/' || req.url === '') {
+      gets.home(req, res);
+    } else {
+      gets.search(req, res);
+    }
+  },
+  'POST': (req, res) => {
+    posts.write(req, res);
+  }
+};
+
+exports.handleRequest = function (req, res) {
+  
+  methods[req.method](req, res);
+
+  // if (req.method === 'GET') {
+  //   if (req.url === '/' || req.url === '') {
+
+  //   } else {
+
+  //   }
+  // } else if (req.method === 'POST') {
+
+    // }); 
 
       // fs.open(archive.paths.list, 'w', (fd) => {
       //   fs.write(fd, body, (fd) => {
@@ -44,6 +66,5 @@ exports.handleRequest = function (req, res) {
       //     fs.close(fd);
       //   });
       // });
-  }
 
 };
