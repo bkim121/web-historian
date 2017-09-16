331c84b6b90ff29af99b0602b314687209c27ad3
bkim121 <bkim121@gmail.com>
2017-09-15 17:08:15 -0700

    Fourth Test

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,21 +1,49 @@
 var path = require('path');
-var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
 var fs = require('fs');
+var parseBody = require('parse-body');
+
+var archive = require('../helpers/archive-helpers');
+// var sendResponse = function(req, res) {
+  
+// };
+
+// var sendFile = function () {
+  
+// };
 
 exports.handleRequest = function (req, res) {
-  if (req.url === '/') {
-    fs.readFile(archive.paths.siteAssets + '/index.html', function (err, data) {
-      res.end(data);  
-    });
-  } else {
-    fs.exists(archive.paths.archivedSites + req.url, (exists) => {
-      if (exists) {
-        fs.readFile(archive.paths.archivedSites + req.url, (err, data) => res.end(data));
-      } else {
-        res.writeHead(404, 'Not Found');
+
+  if (req.method === 'GET') {
+    if (req.url === '/' || req.url === '') {
+      fs.readFile(archive.paths.siteAssets + '/index.html', function (err, data) {
+        res.end(data);  
+      });
+    } else {
+      fs.exists(archive.paths.archivedSites + req.url, (exists) => {
+        if (exists) {
+          fs.readFile(archive.paths.archivedSites + req.url, (err, data) => res.end(data));
+        } else {
+          res.writeHead(404, 'Not Found');
+          res.end();
+        }
+      }); 
+    }
+  } else if (req.method === 'POST') {
+    parseBody(req, 1e6, (err, body) => {
+      fs.writeFile(archive.paths.list, body.url + '\n', () => {
+        res.writeHead(302, 'Found');
         res.end();
-      }
-    });
+      });
+    }); 
+
+      // fs.open(archive.paths.list, 'w', (fd) => {
+      //   fs.write(fd, body, (fd) => {
+      //     res.writeHead(302, 'Found');
+      //     res.end();
+      //     fs.close(fd);
+      //   });
+      // });
   }
+
 };
